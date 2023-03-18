024072b35bfe5c6cdf19640f367ddfaedad6b331
Todd Reifsteck <toddreif@microsoft.com>
2023-03-17 22:44:25 -0700

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,9 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+fetch("file1.json");
+self.addEventListener("fetch", (event) => {
+  fetch("file2.json");
+  event.respondWith(event.request);    
+  });
